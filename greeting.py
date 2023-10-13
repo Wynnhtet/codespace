b1980c7e52a98ab336b597094705422102c7dd28
Wynnhtet <141729846+Wynnhtet@users.noreply.github.com>
2023-10-13 13:29:54 +0000

    Fri, Oct 13, 2023, 1:29 PM +00:00

--- greeting.py
+++ greeting.py
@@ -1,6 +1,7 @@
-name = ["Yoon May Thu","Kitty"]
+name = {"Yoon May Thu","Kitty"}
 
 x_input = input("What is your name? ").lower()
+for n in name :
+    if x_input ==  or x_input == "Kitty" :
+        print(f"Hello : {name} " )
 
-if x_input == "Yoon May Thu" or x_input == "Kitty" :
-    print(f"Hello : {name} " )
