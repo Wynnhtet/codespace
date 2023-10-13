afb45c01ebe1976470d897f8d3959b24f705785a
Wynnhtet <141729846+Wynnhtet@users.noreply.github.com>
2023-10-13 13:15:50 +0000

    Fri, Oct 13, 2023, 1:15 PM +00:00

--- greeting.py
+++ greeting.py
@@ -3,7 +3,7 @@ x2 = "Kitty"
 
 x_input =" What is your name? "
 
-if x_input == x1  :
-    print(f"Hello :{x1}")
-elif x_input == x2 :
-    print (f"Hello :{x2}")
+if x_input == x1:
+    print(f"Hello : ", x1)
+elif x_input == x2:
+    print (f"Hello : ", x2)
