ec612724ea607d8a53e88370f8b9af0b84b9eb32
Wynnhtet <141729846+Wynnhtet@users.noreply.github.com>
2023-10-13 13:21:03 +0000

    Fri, Oct 13, 2023, 1:21 PM +00:00

--- greeting.py
+++ greeting.py
@@ -4,6 +4,6 @@ x2 = "Kitty"
 x_input = input("What is your name? ").lower()
 
 if x_input == x1:
-    print(f"Hello : ", x1)
+    print(f"Hello :{x1} ")
 elif x_input == x2:
-    print (f"Hello : ", x2)
+    print (f"Hello :{x2} " "\n" )
